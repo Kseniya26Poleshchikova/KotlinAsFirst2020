1595914d89396cf4282dfb5ed9aeb46f4650b52d
Kseniya Poleshchikova <poletshikova.kk@edu.spbstu.ru>
2020-09-12 19:37:46 +0300

    Внесены правки по комментариям к заданиям Lesson 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,8 +65,7 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours*3600) + (minutes*60) + seconds
-
+fun seconds(hours: Int, minutes: Int, seconds: Int) = (hours * 3600) + (minutes * 60) + seconds
 /**
  * Тривиальная (1 балл)
  *
@@ -74,7 +73,11 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours*3600) + (minut
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes*0.04445*48) + (arshins*0.04445*16) + (vershoks*0.04445)
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    return ((sagenes * 48) + (arshins * 16) + vershoks) * 0.04445
+}
+
+
 
 /**
  * Тривиальная (1 балл)
@@ -82,7 +85,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg*3600 + min*60 + sec)*(PI/(180*60*60))
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg * 3600 + min * 60 + sec) * (PI / (180 * 60 * 60))
 
 /**
  * Тривиальная (1 балл)
@@ -91,8 +94,8 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg*3600 + min*60 + s
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
 fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
-    val a = (x1-x2)*(x1-x2)
-    val b = (y1-y2)*(y1-y2)
+    val a = (x1 - x2) * (x1 - x2)
+    val b = (y1 - y2) * (y1 - y2)
     val c = a + b
     return sqrt(c)
 }
@@ -104,7 +107,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = (number/100) % 10
+fun thirdDigit(number: Int): Int = (number / 100) % 10
 
 /**
  * Простая (2 балла)
@@ -113,7 +116,9 @@ fun thirdDigit(number: Int): Int = (number/100) % 10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (hoursArrive * 60 + minutesArrive) - (hoursDepart * 60 + minutesDepart)
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    return (hoursArrive * 60 + minutesArrive) - (hoursDepart * 60 + minutesDepart)
+}
 
 /**
  * Простая (2 балла)
@@ -123,9 +128,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
 fun accountInThreeYears(initial: Int, percent: Int): Double {
-    val r = 1 + percent / 100.0
-    val res = initial * (r * r * r)
-    return res
+    val yearPercent = 1 + percent / 100.0
+    return initial * (yearPercent.pow(3))
 }
 
 /**
@@ -135,10 +139,10 @@ fun accountInThreeYears(initial: Int, percent: Int): Double {
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
 fun numberRevert(number: Int): Int {
-    val a = number / 100
-    val b = ((number / 10) % 10) * 10
-    val c = (number % 10) * 100
-    return c + b + a
+    val leastDigit = number / 100
+    val middleDigit = ((number / 10) % 10) * 10
+    val seniorDigit = (number % 10) * 100
+    return seniorDigit + middleDigit + leastDigit
 }
 
 
