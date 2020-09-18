d5d7f8eece351f56bb9a7c8863ea4bafc9ae1b23
Kseniya Poleshchikova <poletshikova.kk@edu.spbstu.ru>
2020-09-18 12:30:03 +0300

    Замечание исправлено

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -94,9 +94,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg * 3600 + min * 60
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
 fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
-    val a = sqr(x1 - x2)
-    val b = sqr(y1 - y2)
-    val c = a + b
+    val c = (sqr(x1 - x2)) + (sqr(y1 - y2))
     return sqrt(c)
 }
 
