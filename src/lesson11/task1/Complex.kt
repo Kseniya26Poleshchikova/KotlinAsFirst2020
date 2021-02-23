69825008bde1c4401de64c558b267881f5cba0c1
Kseniya Poleshchikova <poletshikova.kk@edu.spbstu.ru>
2021-02-23 13:02:52 +0300

    +11

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -2,6 +2,9 @@
 
 package lesson11.task1
 
+import lesson1.task1.sqr
+import java.util.Collections.max
+
 /**
  * Класс "комплексное число".
  *
@@ -11,50 +14,77 @@ package lesson11.task1
  *
  * Аргументы конструктора -- вещественная и мнимая часть числа.
  */
-class Complex(val re: Double, val im: Double) {
+
+class Complex(var re: Double, var im: Double) {
+
 
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, 0.0)
 
     /**
      * Конструктор из строки вида x+yi
      */
-    constructor(s: String) : this(TODO(), TODO())
+    constructor(s: String) : this(re = 0.0, im = 0.0) {
+        when {
+            s.matches(Regex("""^-?\d+(\.\d+)*$""")) -> re = s.toDouble()
+            s.matches(Regex("""^-?\d+(\.\d+)*i$""")) -> im = s.substring(0..s.lastIndex - 1).toDouble()
+            s.matches(Regex("""^-?\d+(\.\d+)*[-+]\d+(\.\d+)*i$""")) -> {
+                val j = s.indexOf('+', 1)
+                val z = s.indexOf('-', 1)
+                val i = if (j > z) j else z
+                re = s.substring(0.. i - 1).toDouble()
+                im = s.substring(i.. s.lastIndex - 1).toDouble()
+            } else -> throw IllegalStateException()
+        }
+    }
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex = Complex(re + other.re, im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus() = Complex(-re, -im)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = Complex(re - other.re, im - other.im)
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex = Complex(re * other.re - im * other.im,
+        re * other.im + im * other.re)
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex = Complex((re * other.re + im * other.im) / (sqr(other.re) + sqr(other.im)),
+        (im * other.re - re * other.im) / (sqr(other.re) + sqr(other.im))
+    )
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is Complex && re == other.re && im == other.im
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String = when {
+        im > 0 -> "$re+${im}i"
+        im < 0 -> "$re${im}i"
+        else -> "$re"
+    }
+
+    override fun hashCode(): Int {
+        var res = re.hashCode()
+        res = res * 31 + im.hashCode()
+        return res
+    }
 }
